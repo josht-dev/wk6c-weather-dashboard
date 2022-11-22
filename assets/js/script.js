feb86da9abffe6531c7787bad605f8675da610a9
Joshua Taylor <toasterrage@gmail.com>
2022-11-21 20:06:12 -0700

    Updated JS to add css class to selected city card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,3 @@
-/*
-*****User Story*****
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-
-*****Acceptance Criteria*****
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that cit
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-*/
-
 // *****Global Variables*****
 // OpenWeather API key
 const oWMApiKey = '13cefbf544495274d042d73ae2f79b5b';
@@ -379,6 +361,15 @@ const globalFunc = {
             htmlCard.getElementsByClassName("temperature")[0].textContent = weatherObj.temp + ' \u00B0' + 'F';
             htmlCard.getElementsByClassName("wind")[0].textContent = weatherObj.wind + ' MPH';
             htmlCard.getElementsByClassName("humidity")[0].textContent = weatherObj.humidity + '%';
+        }
+        // If there is more than one html city card, give border to selected city
+        if (Object.keys(forecasts).length > 1) {
+            for (let x = 0; x < cityList.children.length; x++) {
+                cityList.children[x].classList.remove("selected");
+                if (cityList.children[x].dataset.id == cityId) {
+                    cityList.children[x].classList.add("selected");
+                }
+            }
         }
     }
 };
